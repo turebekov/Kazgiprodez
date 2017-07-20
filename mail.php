f28a33655930691962b076381c4443ab22bf5177
turebekov <turebekov.arman@gmail.com>
2017-07-20 14:15:06 +0600

    end commit

---
+++ mail.php
@@ -0,0 +1,45 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+    }
+    if (isset($_POST['tel'])) {
+        $tel = $_POST['tel'];
+    }
+    if (isset($_POST['organ'])) {
+        $organ = $_POST['organ'];
+    }
+
+    if ($organ == null) {
+        $organ = 'Клиент не заполнил это поле, посчитал что это не обязательно )';
+    }
+    if (isset($_POST['text'])) {
+        $text = $_POST['text'];
+    }
+    if ($text == null) {
+        $text = 'Клиент не заполнил это поле, посчитал что это не обязательно )';
+    }
+
+
+    $subject = "Заявка с сайта";
+    $message = "<h4>Имя клиента: $name</h4><h4>Телефон: $tel</h4><h4>Организация: $organ</h4><h4>Текст:$text</h4>";
+
+
+    require_once "SendMailSmtpClass.php"; // подключаем класс
+
+    $mailSMTP = new SendMailSmtpClass('turebekov.areke@gmail.com', 'thousand', 'ssl://smtp.gmail.com', 'admin', 465);
+// $mailSMTP = new SendMailSmtpClass('логин', 'пароль', 'хост', 'имя отправителя');
+
+// заголовок письма
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
+    $headers .= "From: Admin <admin@vk-book.ru>\r\n"; // от кого письмо
+    $result = $mailSMTP->send('turebekov.areke@gmail.com', 'Заявка с сайта', $message, $headers); // отправляем письмо
+// $result =  $mailSMTP->send('Кому письмо', 'Тема письма', 'Текст письма', 'Заголовки письма');
+    if ($result === true) {
+        echo '<script>alert(\'Ваше  сообщение успешно отправлено\')</script>   ';
+    } else {
+        echo "Письмо не отправлено. Ошибка: " . $result;
+    }
+}
+?>
